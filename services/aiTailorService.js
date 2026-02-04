55bf43081b6f870429ac450134e26305e34d5329
Spencer <sweinstein34@gmail.com>
2026-02-04 09:05:59 -0500

    Cleared repo

--- services/aiTailorService.js
+++
@@ -1,5 +0,0 @@
-const buildTailorPrompt = (resume, jobDescription, instructions) => {};
-
-const buildGeneralPrompt = (resume, instructions) => {};
-
-const runTailorSession = (session, resume, jobApp) => {};
